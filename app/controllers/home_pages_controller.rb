96d852defd269477caaee0143ab178f8dd2b6848
devtuananh <tran.tuan.anhb@framgia.com>
2019-03-03 19:33:10 +0700

    Update code 3.3.19

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,7 +2,7 @@ class HomePagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @my_courses = Course.trainee_courses(current_user.id).page(params[:page])
+    @my_courses = current_user.courses.page(params[:page])
                         .per Settings.per_page
     @supervisor_courses = current_user.courses.page(params[:page])
                                       .per Settings.per_page
